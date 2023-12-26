f04e16e4031b9a28a798673661c0725f123b8999
o.hrachov <ol.grachov@gmail.com>
2023-12-26 20:59:59 +0200

    add mobile version

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 import "./App.scss";
+import {useState, useEffect} from 'react';
 import { CustomersPage } from "./components/CustomersPage";
 import { Navigation } from "./components/Navigation";
 
 function App() {
+  const [isShowMenu, setIsShowMenu] = useState(false);
+  
+  useEffect(() => {
+    const body = document.querySelector('body');
+    if (isShowMenu) {
+      body?.classList.add('lock')
+    } else {
+      body?.classList.remove('lock')
+    }
+  }, [isShowMenu]);
+
   return (
     <div className="app">
-      <Navigation />
+      <div onClick={() => setIsShowMenu(!isShowMenu)} className={`app__menu-btn`}>
+        <div className={`app__menu-btn-item ${isShowMenu ? 'app__menu-btn-item--open' : ''}`}></div>
+        <div className={`app__menu-btn-item ${isShowMenu ? 'app__menu-btn-item--open' : ''}`}></div>
+      </div>
+      <Navigation isShow={isShowMenu} />
       <CustomersPage />
     </div>
   );
